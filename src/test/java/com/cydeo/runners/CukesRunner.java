0bc0b410582bf1ea867376e139490075f0299cad
enestaramis <enes.h.taramis@gmail.com>
2023-03-16 16:19:44 +0000

    parallel testing set up

--- src/test/java/com/cydeo/runners/CukesRunner.java
+++ src/test/java/com/cydeo/runners/CukesRunner.java
@@ -13,7 +13,7 @@ import org.junit.runner.RunWith;
         features ="src/test/resources/features",
         glue = "com/cydeo/step_definitions",
         dryRun = false, //when true results will show snippets that are incomplete
-        tags = "@wip"
+        tags = ""
 )
 public class CukesRunner {
 }
