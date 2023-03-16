44ef29769bf674b14fb33be8c28020f6888af81d
enestaramis <enes.h.taramis@gmail.com>
2023-03-16 14:53:18 +0000

    Rerunner added

---
+++ src/test/java/com/cydeo/runners/FailedTestRunner.java
@@ -0,0 +1,17 @@
+package com.cydeo.runners;
+
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        glue = "com/cydeo/step_definitions",
+        features = "@target/retun.txt"
+
+)
+public class FailedTestRunner {
+
+
+}
